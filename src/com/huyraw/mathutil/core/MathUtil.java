04d1e266bdd423e60471d05ecff42324c1d20472
huy-raw <ruacon130@gmail.com>
2022-05-07 09:57:40 +0700

    add recursion

--- src/com/huyraw/mathutil/core/MathUtil.java
+++ src/com/huyraw/mathutil/core/MathUtil.java
@@ -9,24 +9,43 @@ package com.huyraw.mathutil.core;
  * @author HuyRaw
  */
 public class MathUtil {
-
-    //Viet ham tinh giai thua
-    //0! - 1! = 1
-    //21! tran kieu long
-    // <0 ! khong tinh dc, ko co giai thua am
+    
+    
+    //Recursion - đệ quy 
     public static long getFactorial(int n) {
-        if (n < 0 || n > 20) {
-            throw new IllegalArgumentException("n must be between 0..20");
-        }
-
-        if (n == 0 || n == 1) return 1;
-           
-        long product = 1;
-        for (int i = 2; i <= n; i++) {
-            product *= i;
-        }
-        return product;
+        if (n < 0 || n > 20)
+            throw new IllegalArgumentException("Invalid arg. n must be between 0 -> 20");
+        
+        if (n == 1 || n == 0)
+            return 1;
+        
+        return n*getFactorial(n - 1);
     }
+    
+    
+    
+    
+    
+    
+    
+
+//    //Viet ham tinh giai thua
+//    //0! - 1! = 1
+//    //21! tran kieu long
+//    // <0 ! khong tinh dc, ko co giai thua am
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("n must be between 0..20");
+//        }
+//
+//        if (n == 0 || n == 1) return 1;
+//           
+//        long product = 1;
+//        for (int i = 2; i <= n; i++) {
+//            product *= i;
+//        }
+//        return product;
+//    }
 
 }
 //TDD - Test Driven Development
